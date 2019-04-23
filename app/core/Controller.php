c01d1713008569acd4329540f8e8ebb329299e02
introqt <introtheintro@gmail.com>
2019-04-23 19:48:52 +0300

    index action from main controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,7 +4,16 @@ namespace core;
 
 abstract class Controller {
 
+    /**
+     *
+     * @var Model
+     */
     protected $model;
+
+    /**
+     *
+     * @var View 
+     */
     protected $view;
 
     public function __construct() {
