15a6ffb2e9da29ddb36756031c6e9dd4c7fbde79
introqt <introtheintro@gmail.com>
2019-04-27 09:34:34 +0300

    new question form

--- app/views/questions_index_view.php
+++ app/views/questions_index_view.php
@@ -12,5 +12,10 @@
     </tbody>
 </table> 
 <!--<button id="courses" type="button">Get courses</button>-->
-
+<h2>New question</h2>
+<form name="question">
+    <label>Your name:<input type="text" name="author" required/></label>
+    <label>Your question:<textarea name="area" required></textarea></label>
+    <input type="submit"/>
+</form>
 <script src="js/questions.js"></script>
--- js/questions.js
+++ js/questions.js
@@ -49,3 +49,4 @@ function showQuestions () {
 };
 
 showQuestions();
+
